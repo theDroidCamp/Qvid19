3f8cd4cdc58a20dedf28d2e96976bc908a0d51dd
Noel-elon <noelnwaelugo@gmail.com>
2020-06-08 08:59:32 +0100

    Initia setup

---
+++ app/src/main/java/com/thedroidcamp/qvid19/Constants.kt
@@ -0,0 +1,44 @@
+package com.thedroidcamp.qvid19
+
+const val aboutCovid19 =
+    "Coronavirus disease (COVID-19) is an infectious disease caused by a newly discovered coronavirus.\n" +
+            "Most people who fall sick with COVID-19 will experience mild to moderate symptoms and recover without special treatment.\n" +
+            "HOW IT SPREADS\n" +
+            "The virus that causes COVID-19 is mainly transmitted through droplets generated when an infected person coughs, sneezes, or exhales. These droplets are too heavy to hang in the air, and quickly fall on floors or surfaces.\n" +
+            "You can be infected by breathing in the virus if you are within close proximity of someone who has COVID-19, or by touching a contaminated surface and then your eyes, nose or mouth"
+
+const val preventions =
+    "Protect yourself and others around you by knowing the facts and taking appropriate precautions. Follow advice provided by your local public health agency.\n" +
+            "To prevent the spread of COVID-19:\n" +
+            "Clean your hands often. Use soap and water, or an alcohol-based hand rub.\n" +
+            "Maintain a safe distance from anyone who is coughing or sneezing.\n" +
+            "Don’t touch your eyes, nose or mouth.\n" +
+            "Cover your nose and mouth with your bent elbow or a tissue when you cough or sneeze.\n" +
+            "Stay home if you feel unwell.\n" +
+            "If you have a fever, cough and difficulty breathing, seek medical attention. Call in advance.\n" +
+            "Follow the directions of your local health authority.\n" +
+            "Avoiding unneeded visits to medical facilities allows healthcare systems to operate more effectively, therefore protecting you and others"
+const val symptoms =
+    "COVID-19 affects different people in different ways. Most infected people will develop mild to moderate illness and recover without hospitalization.\n" +
+            "Most common symptoms:\n" +
+            "fever\n" +
+            "dry cough\n" +
+            "tiredness\n" +
+            "Less common symptoms:\n" +
+            "aches and pains\n" +
+            "sore throat\n" +
+            "diarrhoea\n" +
+            "conjunctivitis\n" +
+            "headache\n" +
+            "loss of taste or smell\n" +
+            "a rash on skin, or discolouration of fingers or toes"
+
+const val treatment =
+    "To date, there are no specific vaccines or medicines for COVID-19. Treatments are under investigation, and will be tested through clinical trials. World Health Organization\n" +
+            "Self care\n" +
+            "If you feel sick you should rest, drink plenty of fluid, and eat nutritious food. Stay in a separate room from other family members, and use a dedicated bathroom if possible. Clean and disinfect frequently touched surfaces.\n" +
+            "Everyone should keep a healthy lifestyle at home. Maintain a healthy diet, sleep, stay active, and make social contact with loved ones through the phone or internet. Children need extra love and attention from adults during difficult times. Keep to regular routines and schedules as much as possible.\n" +
+            "It is normal to feel sad, stressed, or confused during a crisis. Talking to people you trust, such as friends and family, can help. If you feel overwhelmed, talk to a health worker or counsellor.\n" +
+            "Medical treatments\n" +
+            "If you have mild symptoms and are otherwise healthy, self-isolate and contact your medical provider or a COVID-19 information line for advice.\n" +
+            "Seek medical care if you have a fever, a cough, and difficulty breathing. Call in advance"
