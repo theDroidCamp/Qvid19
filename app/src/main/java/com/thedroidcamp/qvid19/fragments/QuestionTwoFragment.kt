735dcf386c785605e54aa1a7074c20b2eef59e8b
Junaid Oluwatobi <tobijunaid2@gmail.com>
2020-06-12 23:11:04 +0100

    added kotlin codes

--- app/src/main/java/com/thedroidcamp/qvid19/fragments/QuestionTwoFragment.kt
+++ app/src/main/java/com/thedroidcamp/qvid19/fragments/QuestionTwoFragment.kt
@@ -5,12 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.thedroidcamp.qvid19.R
+import com.thedroidcamp.qvid19.databinding.FragmentQuestionTwoBinding
+import kotlinx.android.synthetic.main.fragment_question_two.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+//private const val ARG_PARAM1 = "param1"
+//private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
@@ -19,26 +22,35 @@ private const val ARG_PARAM2 = "param2"
  */
 class QuestionTwoFragment : Fragment() {
     // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    //private var param1: String? = null
+    //private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    /* override fun onCreate(savedInstanceState: Bundle?) {
+         super.onCreate(savedInstanceState)
+         arguments?.let {
+             param1 = it.getString(ARG_PARAM1)
+             param2 = it.getString(ARG_PARAM2)
+         }
+     }*/
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_question_two, container, false)
+        val binding = DataBindingUtil.inflate<FragmentQuestionTwoBinding>(
+            inflater, R.layout.fragment_question_two, container, false
+        )
+        binding.questionTwo = this
+        binding.invalidateAll()
+        return binding.root
     }
 
-    companion object {
+    fun goToPrevious(){
+        btn_previous.setOnClickListener {  }
+    }
+
+    /*companion object {
         /**
          * Use this factory method to create a new instance of
          * this fragment using the provided parameters.
@@ -56,5 +68,5 @@ class QuestionTwoFragment : Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
-    }
+    }*/
 }
