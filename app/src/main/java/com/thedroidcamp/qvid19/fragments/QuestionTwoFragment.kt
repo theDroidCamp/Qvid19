cf62fdaf93d88c1164fc903493b2f5fafff57e8f
Junaid Oluwatobi <tobijunaid2@gmail.com>
2020-06-13 20:39:02 +0100

    some final touches

--- app/src/main/java/com/thedroidcamp/qvid19/fragments/QuestionTwoFragment.kt
+++ app/src/main/java/com/thedroidcamp/qvid19/fragments/QuestionTwoFragment.kt
@@ -5,14 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-<<<<<<< HEAD
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
-=======
 import android.widget.Toast
 import com.thedroidcamp.qvid19.MainActivity
->>>>>>> upstream/master
 import com.thedroidcamp.qvid19.R
 import com.thedroidcamp.qvid19.databinding.FragmentQuestionTwoBinding
 import kotlinx.android.synthetic.main.fragment_question_two.*
@@ -31,36 +28,59 @@ import kotlinx.android.synthetic.main.fragment_question_two.*
 class QuestionTwoFragment : Fragment() {
 
     lateinit var navController: NavController
+    private lateinit var _binding: FragmentQuestionTwoBinding
+    private lateinit var mainActivity: MainActivity
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val binding = DataBindingUtil.inflate<FragmentQuestionTwoBinding>(
+        mainActivity =
+            context as MainActivity // doing this because running i don't know about how to pass the score to next fragment
+        if (mainActivity.q2Answered) {
+            _binding.answerGroup.isEnabled = false
+            setAllRadioButtonDisable()
+        }
+        _binding = DataBindingUtil.inflate<FragmentQuestionTwoBinding>(
             inflater, R.layout.fragment_question_two, container, false
         )
-
-        binding.btnNext.setOnClickListener { goToNext() }
-        binding.btnPrevious.setOnClickListener { goToPrevious() }
-        binding.questionTwo = this
-        binding.invalidateAll()
-        return binding.root
+        _binding.questionTwo = this
+        _binding.invalidateAll()
+        return _binding.root
     }
 
-   private fun goToNext(){
+    private fun setAllRadioButtonDisable() {
+        _binding.optionOneRadioBtn.isEnabled = false
+        _binding.optionTwoRadioBtn.isEnabled = false
+        _binding.optionThreeRadioBtn.isEnabled = false
+
+    }
+
+    private fun goToNext() {
         navController.navigate(QuestionTwoFragmentDirections.actionQuestionTwoFragmentToQuestionThreeFragment())
+        when (_binding.answerGroup.checkedRadioButtonId) {
+            -1 -> return
+            _binding.optionThreeRadioBtn.id -> addUpScore()
+        }
+        //save a state that question one answered already
+        mainActivity.setAnswered(2) //setting answer id 1 as answered
     }
 
-   private fun goToPrevious(){
-        navController.navigate(QuestionTwoFragmentDirections.actionQuestionTwoFragmentToQuestionOneFragment())
+    private fun goToPrevious() {
+        navController.navigate(QuestionTwoFragmentDirections.actionQuestionTwoFragmentToQuestionOneFragment2())
+    }
+
+    private fun addUpScore() {
+        mainActivity.calculateScore(10) //giving 10 point for correct answer
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         navController = Navigation.findNavController(view)
-
-
+        _binding.scoreView.text = mainActivity.currentScore.toString()
+        _binding.nextLink.setOnClickListener { goToNext() }
+        _binding.previousLink.setOnClickListener { goToPrevious() }
     }
 
 
