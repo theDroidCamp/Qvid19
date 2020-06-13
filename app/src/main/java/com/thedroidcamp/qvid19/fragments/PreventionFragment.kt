5d6743855c4c6b8cde574835f0f9558c88d3ef0e
Ipshita Roy Burman <ipshitarburman@gmail.com>
2020-06-13 10:12:26 -0400

    Revert "Merging from rexfilius/Qvid19"

--- app/src/main/java/com/thedroidcamp/qvid19/fragments/PreventionFragment.kt
+++ app/src/main/java/com/thedroidcamp/qvid19/fragments/PreventionFragment.kt
@@ -5,37 +5,56 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
-import androidx.navigation.Navigation
-import androidx.navigation.findNavController
 import com.thedroidcamp.qvid19.R
-import com.thedroidcamp.qvid19.databinding.FragmentDashboardBinding
-import com.thedroidcamp.qvid19.databinding.FragmentPreventionBinding
-import com.thedroidcamp.qvid19.preventions
-import kotlinx.android.synthetic.main.fragment_prevention.*
 
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [PreventionFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
 class PreventionFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
 
-    private lateinit var binding: FragmentPreventionBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        
-         binding = DataBindingUtil.inflate(inflater,
-            R.layout.fragment_prevention, container, false)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_prevention, container, false)
+    }
 
-        prevention_fragment_text.text = preventions
-
-        prevention_fragment_previous_btn.setOnClickListener(
-            Navigation.createNavigateOnClickListener(
-                R.id.action_preventionFragment_to_symptomsFragment)
-        )
-
-        prevention_fragment_next_btn.setOnClickListener(
-            Navigation.createNavigateOnClickListener(
-                R.id.action_preventionFragment_to_treatmentsFragment)
-        )
-
-        return binding.root
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment PreventionFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            PreventionFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
     }
 }
