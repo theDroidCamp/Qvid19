dae306b5e992e9efa5ac0faff0f2f8373622790a
Benedict-Ik <benedict.ik98@gmail.com>
2020-06-14 14:46:29 +0100

    initial commit

--- app/src/main/java/com/thedroidcamp/qvid19/fragments/TreatmentsFragment.kt
+++ app/src/main/java/com/thedroidcamp/qvid19/fragments/TreatmentsFragment.kt
@@ -5,7 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
 import com.thedroidcamp.qvid19.R
+import com.thedroidcamp.qvid19.databinding.FragmentTreatmentsBinding
+import com.thedroidcamp.qvid19.treatment
+import kotlinx.android.synthetic.main.fragment_treatments.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,43 +23,27 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class TreatmentsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentTreatmentsBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_treatments, container, false)
-    }
+        savedInstanceState: Bundle?): View? {
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment TreatmentsFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            TreatmentsFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        binding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_treatments, container, false)
+
+        treatments_fragment_text.text = treatment
+
+        binding.treatmentsFragmentPreviousButton.setOnClickListener{ view: View ->
+            Navigation.findNavController(view).navigate(R.id.action_treatmentsFragment_to_preventionFragment)
+        }
+
+        binding.treatmentsFragmentNextButton.setOnClickListener{
+            Navigation.createNavigateOnClickListener(R.id.action_treatmentsFragment_to_hoorayFragment)
+        }
+
+
+        return binding.root
     }
 }
