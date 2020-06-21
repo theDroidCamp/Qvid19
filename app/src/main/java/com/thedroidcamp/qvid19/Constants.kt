6668614a9610ca218f2fe2656541fe2f27123928
descobee <61969550+descobee@users.noreply.github.com>
2020-06-21 09:13:54 +0100

    The creation of the About Fragment

--- app/src/main/java/com/thedroidcamp/qvid19/Constants.kt
+++ app/src/main/java/com/thedroidcamp/qvid19/Constants.kt
@@ -2,7 +2,7 @@ package com.thedroidcamp.qvid19
 
 const val aboutCovid19 =
     "Coronavirus disease (COVID-19) is an infectious disease caused by a newly discovered coronavirus.\n" +
-            "Most people who fall sick with COVID-19 will experience mild to moderate symptoms and recover without special treatment.\n" +
+            "Most people who fall sick with COVID-19 will experience mild to moderate symptoms and recover without special treatment.\n\n" +
             "HOW IT SPREADS\n" +
             "The virus that causes COVID-19 is mainly transmitted through droplets generated when an infected person coughs, sneezes, or exhales. These droplets are too heavy to hang in the air, and quickly fall on floors or surfaces.\n" +
             "You can be infected by breathing in the virus if you are within close proximity of someone who has COVID-19, or by touching a contaminated surface and then your eyes, nose or mouth"
